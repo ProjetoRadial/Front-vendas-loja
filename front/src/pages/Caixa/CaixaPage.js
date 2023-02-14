2ac6e6a7b3bed5bd8b33d0a29d5870e12d776cab
lsmarrano <lsmarrano@gmail.com>
2023-02-14 11:07:56 -0300

    inicianco caixaPage

--- front/src/pages/Caixa/CaixaPage.js
+++ front/src/pages/Caixa/CaixaPage.js
@@ -1,27 +1,111 @@
-import Sidebar from "../../components/Sidebar/Sidebar";
+//simple cashier app with in className bootstrap 
 
-const CaixaPage = () => {
-    return (
-        <section>
-            <Sidebar />
-            <div className="text-center pt-2" style={{ "paddingLeft": "260px",  }}>
-                <h1>Caixa</h1>
-                <div className="container d-flex ml-2">
-                    <div className="col-8 " style={{ "backgroundColor": "black" }}>
-                        {/*List all the products inside the cashier*/}
-                        <h1 className="pb-4">Lista de Produtos</h1>
-                        <h3>Cartão TOP</h3>
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import OnpagLogo from "../../images/logoOnpag.png"
+import ProdutosIcon from "../../images/produtos-icon.png"
 
+//products list mock
+const products = [
+    {
+        id: 1,
+        nome: 'Coca-cola',
+        preco: 5.00
+    },
+    {
+        id: 2,
+        nome: 'Fanta',
+        preco: 5.00
+    },
+    {
+        id: 3,
+        nome: 'Sprite',
+        preco: 5.00
+    }
+
+]
+
+
+export const CaixaPage = () => {
+    return (<div className='vh-100 text-center p-1'>
+        <div className='card h-100'>
+            <div className='card-body flex h-100'>
+                <div className='row h-100' style={{ "width": "140px" }}>
+                    <div className='flex align-items-center justify-content-center p-1'>
+                        <a className='d-block text-decoration-none' href='/'>
+                            <div className='flex align-items-center justify-content-center'>
+                                <img src={OnpagLogo} style={{ "height": "40px" }} alt='Onpag Logo'></img>
+                            </div>
+                            <div className='fs-4 text-black p-10'>
+                                <h3 className='pt-2'>CAIXA</h3>
+                            </div>
+
+                        </a>
                     </div>
-                    <div className="col-4" style={{ "backgroundColor": "red" }}>
-                        {/*List all the products inside the cashier*/}
-                        <h1>Teste</h1>
+                    <div className='nav-container overflow-hidden' style={{"backgroundColor":"black"}}>
+                        <div className='position-relative h-100'>
+                            <ul className='nav nav-tabs'>
+                                <li className='nav-item'>
+                                    <a className='nav-link active' href='#'>
+                                        <div className='card'>
+                                            <div className='card-body' style={{"backgroundColor": "gold"}}>
+                                                <img src={ProdutosIcon} style={{ "height": "40px" }} alt='Produtos Icon'></img>
+                                                <h5 className='card-title'>Todos os Produtos</h5>
+                                            </div>
+                                            
+                                        </div>
+                                    </a>
+                                </li> <li className='nav-item'>
+                                    <a className='nav-link active' href='#'>
+                                        <div className='card'>
+                                            <div className='card-body'>
+                                                <img src={ProdutosIcon} style={{ "height": "40px" }} alt='Produtos Icon'></img>
+                                                <h5 className='card-title'>Todos os Produtos</h5>
+                                            </div>
+                                            
+                                        </div>
+                                    </a>
+                                </li> <li className='nav-item'>
+                                    <a className='nav-link active' href='#'>
+                                        <div className='card'>
+                                            <div className='card-body'>
+                                                <img src={ProdutosIcon} style={{ "height": "40px" }} alt='Produtos Icon'></img>
+                                                <h5 className='card-title'>Todos os Produtos</h5>
+                                            </div>
+                                            
+                                        </div>
+                                    </a>
+                                </li> <li className='nav-item'>
+                                    <a className='nav-link active' href='#'>
+                                        <div className='card'>
+                                            <div className='card-body'>
+                                                <img src={ProdutosIcon} style={{ "height": "40px" }} alt='Produtos Icon'></img>
+                                                <h5 className='card-title'>Todos os Produtos</h5>
+                                            </div>
+                                            
+                                        </div>
+                                    </a>
+                                </li>
+                                <li className='nav-item'>
+                                </li>
+                                <li className='nav-item'>
+                                </li>
+                                <li className='nav-item'>
+                                </li>
+                                <li className='nav-item'>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
+
+
             </div>
 
-        </section>
-    );
+        </div>
+    </div>
+    )
 }
 
-export default CaixaPage;
+export default CaixaPage
