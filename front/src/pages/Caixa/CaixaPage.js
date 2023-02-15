276411544e694d1bafd65a0568f1d298f4fd1bf6
lsmarrano <lsmarrano@gmail.com>
2023-02-15 15:18:57 -0300

    esqueci hehe

--- front/src/pages/Caixa/CaixaPage.js
+++ front/src/pages/Caixa/CaixaPage.js
@@ -1,72 +1,85 @@
 //simple cashier app with in className bootstrap 
 
 import React from 'react'
+import Cartao from '../../components/Cartao/Cartao'
 import './CaixaPage.css'
 import { useState } from 'react'
-import { useEffect } from 'react'
 import OnpagLogo from "../../images/logoOnpag.png"
-import ProdutosIcon from "../../images/produtos-icon.png"
-import CartaoTop from "../../images/cartao-top.png"
 import CartaoComum from "../../images/cartao-comum.png"
 import CartaoSenior from "../../images/cartao-senior.png"
 import CartaoVT from "../../images/cartao-vt.png"
 import CartaoEscolar from "../../images/cartao-escolar.png"
 
-const produtos = [
-    {
-        id: 1,
-        nome: 'Cartão comum',
-        imagem: CartaoComum,
-        preco: {
-            'primeiraVia': 0,
-            'segundaVia': 26.50,
-            'recarga': 0
-        }
-    },
-    {
-        id: 2,
-        nome: 'Cartão senior',
-        imagem: CartaoSenior,
-        preco: {
-            'primeiraVia': 0,
-            'segundaVia': 26.50,
-            'recarga': 0
-        }
-    },
-    {
-        id: 3,
-        nome: 'Cartão VT',
-        imagem: CartaoVT,
-        preco: {
-            'primeiraVia': 0,
-            'segundaVia': 26.50,
-            'recarga': 0
-        }
-    },
-    {
-        id: 4,
-        nome: 'Cartão escolar',
-        imagem: CartaoEscolar,
-        preco: {
-            'primeiraVia': 0,
-            'segundaVia': 26.50,
-            'recarga': 0
-        }
-    }
-]
-
 export const CaixaPage = () => {
-    const [produtosCaixa, setProdutosCaixa] = useState([])
 
-    useEffect(() => {
-        const produto = produtos.map(produto => {
-            return {
-                ...produto,
-                quantidade: 1
+    const produtos = [
+        {
+            id: 1,
+            nome: 'Cartão comum',
+            imagem: CartaoComum,
+            preco: {
+                '1ª via': 0,
+                '2ª via': 26.50,
+                'Recarga': 0
             }
-        })
-        setProdutosCaixa(produto)
-    }, [])
+        },
+        {
+            id: 2,
+            nome: 'Cartão senior',
+            imagem: CartaoSenior,
+            preco: {
+                '1ª via': 0,
+                '2ª via': 26.50,
+                'Recarga': 0
+            }
+        },
+        {
+            id: 3,
+            nome: 'Cartão VT',
+            imagem: CartaoVT,
+            preco: {
+                '1ª via': 0,
+                '2ª via': 26.50,
+                'Recarga': 0
+            }
+        },
+        {
+            id: 4,
+            nome: 'Cartão escolar',
+            imagem: CartaoEscolar,
+            preco: {
+                '1ª via': 0,
+                '2ª via': 26.50,
+                'Recarga': 0
+            }
+        }
+    ]
+
+    const [produtosCaixa, setProdutosCaixa] = useState([])
+    const [precoServico, setPrecoServico] = useState([])
+
+
+
+    const adicionarProduto = (produto) => {
+        setProdutosCaixa([...produtosCaixa, produto])
+        console.log(produtosCaixa)
+    }
+
+    const selecionarServico = (produto, servico) => {
+        adicionarProduto(produto)
+        setPrecoServico([...precoServico, produto.preco[servico]])
+        console.log(precoServico)
+    }
+
+    const removeProduto = (produto) => {
+        setProdutosCaixa(produtosCaixa.filter(p => p.id !== produto.id))
+    }
+
+    
+
+
+
+
 
 
 
@@ -80,76 +93,14 @@ export const CaixaPage = () => {
                     <div className='row h-100' style={{ 'backgroundColor': '#f8f8f8' }}>
 
                         <div className='col-9 d-flex flex-wrap justify-content-between p-2' >
-                            <div className="card m-3" style={{ "width": "18rem", 'backgroundColor': '#f8f8f8' }}>
-                                <img className="card-img-top p-2" src={CartaoTop} alt="teste" />
-                                <div className="card-body d-flex justify-content-between align-items-center">
-                                    <h5 className="card-title">Cartão TOP</h5>
-                                    <a href="/" class="btn btn-primary">Selecionar</a>
-                                </div>
-                            </div>
 
-                            <div className="card m-3" style={{ "width": "18rem", }} >
-                                <img className="card-img-top p-2" src={CartaoComum} alt="teste" />
-                                <div className="card-body d-flex justify-content-between align-items-center">
-                                    <h5 className="card-title">Cartão comum</h5>
-                                    <a href="/" class="btn btn-primary">Selecionar</a>
-                                </div>
-                            </div>
 
-                            <div className="card m-3" style={{ "width": "18rem", 'backgroundColor': '#f8f8f8' }}>
-                                <img className="card-img-top p-2" src={CartaoSenior} alt="teste" />
-                                <div className="card-body d-flex justify-content-between align-items-center">
-                                    <h5 className="card-title">Cartão senior</h5>
-                                    <a href="/" >
-                                        <button type="button" class="btn btn-primary btn-rounded">Primary</button>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="card m-3" style={{ "width": "18rem", 'backgroundColor': '#f8f8f8' }}>
-                                <img className="card-img-top p-2" src={CartaoVT} alt="teste" />
-                                <div className="card-body d-flex justify-content-between align-items-center">
-                                    <h5 className="card-title">Cartão VT</h5>
-                                    <a href="/" class="btn btn-primary">Selecionar</a>
-                                </div>
-                            </div>
-                            <div className="card m-3" style={{ "width": "18rem", 'backgroundColor': '#f8f8f8' }}>
-                                <img className="card-img-top p-2" src={CartaoEscolar} alt="teste" />
-                                <div className="card-body d-flex justify-content-between align-items-center">
-                                    <h5 className="card-title">Cartão Escolar</h5>
-                                    <a href="/" class="btn btn-primary">Selecionar</a>
-                                </div>
-                            </div>
-                            <div className="container">
-                                <div className="row">
 
-                                    <div className="col-md-4 col-sm-6">
-                                        <div className="box">
-                                            <img src={CartaoComum} alt='Cartao comum' />
-                                            <div className="box-content">
-                                                <h3 className="title">Cartão Comum</h3>
+                            {produtos.map((produto) => (
 
-                                                <ul className="icon">
-                                                    <li href='#' ><a href='#'><i className="fa fa-search"></i></a></li>
-                                                    <li ><i className="fa fa-link"></i></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4 col-sm-6">
-                                        <div class="box">
-                                            <img src={CartaoEscolar}/>
-                                                <div class="box-content">
-                                                    <h3 class="title">Cartão escolar</h3>
-                                                    
-                                                    <ul class="icon">
-                                                        <li onClick={() => setProdutosCaixa(produtos[1])}><a href="#"><i class="fa fa-search"></i></a></li>
-                                                        <li onClick={() => console.log(produtosCaixa)}><a href="#"><i class="fa fa-link"></i></a></li>
-                                                    </ul>
-                                                </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                                <Cartao key={produto.id} produto={produto} onSelecionarServico={selecionarServico}/>
+                            ))}
+
 
                         </div>
                         <div className='col-3 border' >
@@ -201,6 +152,7 @@ export const CaixaPage = () => {
             </div>
         </div>
 
+        
     )
 }
 
